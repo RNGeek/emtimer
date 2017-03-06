1b6700b4804821ca273a8d80b362ddd593eab17c
mizdra <pp.mizdra@gmail.com>
2017-03-07 01:42:51 +0900

    improve eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'object-shorthand': ['off', 'properties'],
-    'func-names': ["error", "as-needed"]
+    'func-names': ["error", "as-needed"],
+    'max-len': ["off"]
   }
 }
