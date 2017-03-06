b195445d25f04d4d13919bdbddc9a9d9798f8e6e
mizdra <pp.mizdra@gmail.com>
2017-03-06 22:37:48 +0900

    fix typo

--- src/components/TimeView.vue
+++ src/components/TimeView.vue
@@ -1,7 +1,7 @@
 <template>
   <span>
-    {{ days }}日
-    {{ hour | pad }}時間
+    <span v-if="days > 0">{{ days }}日</span>
+    <span v-if="hours > 0">{{ hours }}時間</span>
     {{ minutes | pad }}分
     {{ seconds | pad }}秒
     {{ cs | pad }}
@@ -14,7 +14,7 @@ export default {
   props: ['value'],
   computed: {
     days() { return Math.trunc(this.value / 1000 / 60 / 60 / 24); },
-    hour() { return Math.trunc(this.value / 1000 / 60 / 60) % 24; },
+    hours() { return Math.trunc(this.value / 1000 / 60 / 60) % 24; },
     minutes() { return Math.trunc(this.value / 1000 / 60) % 60; },
     seconds() { return Math.trunc(this.value / 1000) % 60; },
     cs() { return Math.trunc(this.value / 10) % 100; },
