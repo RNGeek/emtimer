71780aeaf25d694718c1d9c25ed151df6c2f7c97
mizdra <pp.mizdra@gmail.com>
2017-03-07 22:06:11 +0900

    support resuming and pausing

--- src/components/Emtimer.vue
+++ src/components/Emtimer.vue
@@ -4,6 +4,8 @@
     <div>
       <button v-if="!started" @click="start">開始</button>
       <button v-else @click="stop">停止</button>
+      <button @click="resume">再開</button>
+      <button @click="pause">一時停止</button>
     </div>
     <time-view :value="remainingDuration_ms" />
   </div>
@@ -22,45 +24,46 @@ export default {
   },
   data() {
     return {
-      startTime_ms: 0,
-      nowTime_ms: 0,
-      duration_ms: 0,
+      remainingDuration_ms: 0,
       started: false,
       inputDuration_s: 10,
     };
   },
-  computed: {
-    remainingDuration_ms() { return Math.max(this.duration_ms - (this.nowTime_ms - this.startTime_ms), 0); },
-  },
   methods: {
+    startRAFLoop() {
+      const cb = (newTimestamp_ms, oldTimestamp_ms) => {
+        if (this.remainingDuration_ms === 0 || !this.started) {
+          this.started = false;
+          return;
+        }
+        this.remainingDuration_ms = Math.max(this.remainingDuration_ms - (newTimestamp_ms - oldTimestamp_ms), 0);
+        raf(timestamp_ms => cb(timestamp_ms, newTimestamp_ms));
+      };
+      raf(timestamp_ms => cb(timestamp_ms, window.performance.now()));
+    },
     start() {
-      const initProps = () => {
-        this.startTime_ms = window.performance.now();
-        this.nowTime_ms = this.startTime_ms;
-        this.duration_ms = this.inputDuration_s * 1000;
-        this.started = true;
-      };
-      const startRAF = () => {
-        const cb = (timestamp_ms) => {
-          if (this.remainingDuration_ms === 0 || !this.started) {
-            this.started = false;
-            return;
-          }
-          this.nowTime_ms = timestamp_ms;
-          raf(cb);
-        };
-        raf(cb);
-      };
-
+      // 既に RAF loop が起動している場合は RAF loop を停止せずにパラメータのみを変更する
       if (this.started) {
-        initProps();
+        this.duration_ms = this.inputDuration_s * 1000;
+        this.remainingDuration_ms = this.duration_ms;
       } else {
-        initProps();
-        startRAF();
+        this.duration_ms = this.inputDuration_s * 1000;
+        this.remainingDuration_ms = this.duration_ms;
+        this.started = true;
+        this.startRAFLoop();
       }
     },
     stop() {
-      this.duration_ms = 0; // Clear props
+      this.duration_ms = 0;
+      this.remainingDuration_ms = 0;
+      this.started = false;
+    },
+    resume() {
+      this.started = true;
+      this.startRAFLoop();
+    },
+    pause() {
+      this.started = false;
     },
   },
   deactivated() {
