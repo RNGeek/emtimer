fcf59b4b4dd60f78e822fa1a226663d46ff49c53
mizdra <pp.mizdra@gmail.com>
2017-03-07 22:17:45 +0900

    refactor

--- src/components/Emtimer.vue
+++ src/components/Emtimer.vue
@@ -39,7 +39,11 @@ export default {
         this.remainingDuration_ms = Math.max(this.remainingDuration_ms - (newTimestamp_ms - oldTimestamp_ms), 0);
         raf(timestamp_ms => cb(timestamp_ms, newTimestamp_ms));
       };
+      this.started = true;
       raf(timestamp_ms => cb(timestamp_ms, window.performance.now()));
+    },
+    stopRADLoop() {
+      this.started = false;
     },
     start() {
       // 既に RAF loop が起動している場合は RAF loop を停止せずにパラメータのみを変更する
@@ -49,21 +53,19 @@ export default {
       } else {
         this.duration_ms = this.inputDuration_s * 1000;
         this.remainingDuration_ms = this.duration_ms;
-        this.started = true;
         this.startRAFLoop();
       }
     },
     stop() {
       this.duration_ms = 0;
       this.remainingDuration_ms = 0;
-      this.started = false;
+      this.stopRADLoop();
     },
     resume() {
-      this.started = true;
       this.startRAFLoop();
     },
     pause() {
-      this.started = false;
+      this.stopRADLoop();
     },
   },
   deactivated() {
