da08f6aafcfe2638509cc0a158986db6360b030d
mizdra <pp.mizdra@gmail.com>
2017-03-09 19:15:56 +0900

    support Muse-UI

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
 import Emtimer from './components/Emtimer';
+
+Vue.use(MuseUI);
 
 export default {
   name: 'app',
