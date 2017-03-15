c98cf674a22e142b7f326ebe33257bbad64f9d1e
mizdra <pp.mizdra@gmail.com>
2017-03-15 22:42:09 +0900

    add `duration-input`

--- src/components/Emtimer.vue
+++ src/components/Emtimer.vue
@@ -4,14 +4,7 @@
     <mu-card class="config-card" :zDepth="1">
       <mu-card-title class="config-title" title="設定" />
       <mu-card-text>
-        <mu-flexbox class="duration-group" align="flex-end">
-          <mu-flexbox-item>
-            <mu-text-field fullWidth v-model.number="durations.main.value" :errorText="getErrorText('main')" label="時間" hintText="10" />
-          </mu-flexbox-item>
-          <mu-flexbox-item>
-            <unit-select class="unit-select" v-model="durations.main.unit" />
-          </mu-flexbox-item>
-        </mu-flexbox>
+        <duration-input label="時間" hintText="0" @input="value => mainDuration = value" />
       </mu-card-text>
       <mu-card-actions>
         <mu-flat-button @click="stop()" label="停止" icon="stop" color="#e36209" />
@@ -28,14 +21,11 @@
 <script>
 /* eslint-disable camelcase */
 import { validationMixin } from 'vuelidate';
-import { required, between } from 'vuelidate/lib/validators';
 import { card, cardTitle, cardText, cardActions } from 'muse-ui/src/card';
-import { flexbox, flexboxItem } from 'muse-ui/src/flexbox';
-import textField from 'muse-ui/src/textField';
 import flatButton from 'muse-ui/src/flatButton';
 import CountdownTimer from './CountdownTimer';
 import UnitSelect from './UnitSelect';
-import { parseDuration } from '../lib/math';
+import DurationInput from './DurationInput';
 
 export default {
   name: 'emtimer',
@@ -47,27 +37,20 @@ export default {
     cardTitle,
     cardText,
     cardActions,
-    flexbox,
-    flexboxItem,
-    textField,
+    DurationInput,
     flatButton,
   },
   data() {
     return {
       paused: true,
       ended: true,
-      durations: {
-        main: { value: 10, unit: 's' },
-      },
+      mainDuration: 0,
     };
   },
   validations: {
-    durations: {
-      main: {
-        value: {
-          required,
-          between: between(0, 10000000000),
-        },
+    mainDuration: {
+      notNaN(value) {
+        return !Number.isNaN(value);
       },
     },
   },
@@ -75,9 +58,7 @@ export default {
     start() {
       if (this.$v.$invalid) return;
 
-      const { value, unit } = this.durations.main;
-      const duration_ms = parseDuration(value, unit);
-      this.$refs.timer.start(duration_ms);
+      this.$refs.timer.start(this.mainDuration);
     },
     stop() {
       this.$refs.timer.stop();
@@ -91,17 +72,6 @@ export default {
     updateState() {
       this.paused = this.$refs.timer.paused;
       this.ended = this.$refs.timer.ended;
-    },
-    getErrorText(durationName) {
-      const valid = this.$v.durations[durationName];
-      let text = '';
-      if (!valid.value.required) {
-        text += '時間は必須です. ';
-      }
-      if (!valid.value.between) {
-        text += '時間は 0 以上 10,000,000,000 以下の数値でなければなりません. ';
-      }
-      return text;
     },
   },
   deactivated() {
