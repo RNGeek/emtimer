2d7d4e78b0d2bfb4e4313cbfd1866cbf8d2089e3
mizdra <pp.mizdra@gmail.com>
2017-08-27 16:13:02 +0900

    add AppFooter

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <div>
     <app-bar />
     <emtimer />
+    <app-footer />
   </div>
 </template>
 
 <script>
 import AppBar from './components/AppBar';
 import Emtimer from './components/Emtimer';
+import AppFooter from './components/AppFooter';
 
 export default {
   name: 'app',
   components: {
     AppBar,
     Emtimer,
+    AppFooter,
   },
 };
 </script>
-
-<style>
-</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="footer">
+    &copy; 2017 <a href="https://twitter.com/mizdra" target="_blank">mizdra</a>.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-footer',
+};
+</script>
+
+<style scoped>
+.footer {
+  background-color: #f9f9f9;
+  text-align: center;
+  border-top: 1px #ddd solid;
+  margin-top: 30px;
+  padding: 20px;
+}
+</style>
