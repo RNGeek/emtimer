f9d98ff58357fed18d14c07b705a99b1e16c847a
mizdra <pp.mizdra@gmail.com>
2017-08-28 19:34:44 +0900

    change title

--- src/router.js
+++ src/router.js
@@ -2,20 +2,30 @@ import VueRouter from 'vue-router';
 import SimpleTimer from './pages/SimpleTimer';
 import About from './pages/About';
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   routes: [
     {
       path: '/SimpleTimer',
       component: SimpleTimer,
+      meta: { title: 'シンプルタイマー · エメタイマー' },
     },
     {
       path: '/About',
       component: About,
+      meta: { title: 'About · エメタイマー' },
     },
     {
       path: '/',
       component: SimpleTimer,
+      meta: { title: 'シンプルタイマー · エメタイマー' },
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default router;
