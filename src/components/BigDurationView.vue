548fb165a11c5450bc47629289d4a3ad58e911d3
mizdra <pp.mizdra@gmail.com>
2017-08-28 15:33:10 +0900

    support highlight

--- src/components/BigDurationView.vue
+++ src/components/BigDurationView.vue
@@ -1,24 +1,55 @@
 <template>
-  <div class="view">
+  <div class="view" :style="{ background: bgColor, transitionDuration: duration }" >
     <div class="line"></div>
     <div class="circle" :style="{ right: right }"></div>
   </div>
 </template>
 
 <script>
+/**
+ * 赤丸の位置で秒以下の桁を表わす.
+ * 秒の桁が変わる瞬間に赤丸が中央を通過し, 通過する際にハイライトする.
+ */
+
+/* eslint-disable camelcase */
 import DurationView from './DurationView';
+import { canTicktack } from '../lib/util';
 
 export default {
   name: 'big-duration-view',
   extends: DurationView,
+  data() {
+    return {
+      beforeDuration: 0,
+      bgColor: 'transparent',
+      duration: '0ms',
+    };
+  },
   computed: {
     right() {
       const cs = ((this.value / 10) % 100); // centi seconds
       return `${(((cs + 37.5) * 2) % 200) - 25}%`;
     },
   },
+  watch: {
+    value() {
+      // 秒の桁が切り替わる時, ハイライトする
+      if (canTicktack(this.value, this.beforeDuration)) {
+        this.bgColor = '#ffff00';
+        this.duration = '0ms'; // CSS Transitionを無効化
+
+        // 本来はVue#$nextTickを使うべきだが, 上手く動かないのでsetTimeoutで代用
+        setTimeout(() => {
+          // ハイライトしたらCSS Transitionを有効化しつつすぐに元の状態に戻す
+          this.bgColor = 'transparent';
+          this.duration = '500ms'; // CSS Transitionを有効化
+        }, 0);
+      }
+      this.beforeDuration = this.value;
+    },
+  },
 };
-</script>gyhjnk
+</script>
 
 <style scoped>
 :root {
@@ -30,6 +61,10 @@ export default {
   position: relative;
   height: calc(var(--circle-size) * 2);
   overflow-x: hidden;
+  transition: all 0ms 0ms ease;
+  box-sizing: content-box;
+  border: 1px #ddd solid;
+  border-radius: 3px;
 }
 
 .line {
