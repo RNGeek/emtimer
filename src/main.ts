e319ae7906b70a68d592b0930d4277c69d717283
mizdra <pp.mizdra@gmail.com>
2017-12-18 01:16:44 +0900

    add tslib

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
+import 'tslib'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MuseUI from 'muse-ui'
