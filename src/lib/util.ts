9091ac8a8d28cfdbca167e2fc460ff28714c7662
mizdra <pp.mizdra@gmail.com>
2020-03-29 19:11:00 +0900

    未使用のファイルを削除

--- src/lib/util.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable camelcase */
-
-// 秒が切り替わる瞬間であればtrueを返す
-export const canTicktack = (newDuration: number, oldDuration: number) => {
-  const newDuration_s = Math.ceil(newDuration / 1000);
-  const oldDuration_s = Math.floor(oldDuration / 1000);
-  return newDuration_s <= oldDuration_s;
-};
