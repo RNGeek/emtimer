37140bd69c0cc2bf05888b1c9ad8fe61eeb266e9
mizdra <pp.mizdra@gmail.com>
2020-03-29 23:01:31 +0900

    stopを実装

--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -3,16 +3,23 @@ import EventEmitter from 'eventemitter3';
 type EventTypes = {
   onstart: [number];
   onend: [];
+  onstop: [];
   ontick: [number];
 };
 
 export class Timer {
   #emitter: EventEmitter<EventTypes>;
   #endTime: number;
+  #rafId: number | null;
 
   constructor() {
     this.#emitter = new EventEmitter();
     this.#endTime = 0;
+    this.#rafId = null;
+  }
+
+  get isEnded(): boolean {
+    return this.#rafId === null;
   }
 
   start(duration: number) {
@@ -20,19 +27,26 @@ export class Timer {
       const remainingDuration = this.#endTime - Date.now();
 
       if (remainingDuration > 0) {
-        requestAnimationFrame(updateDuration);
+        this.#rafId = requestAnimationFrame(updateDuration);
         this.#emitter.emit('ontick', remainingDuration);
       } else {
+        this.#rafId = null;
         this.#emitter.emit('onend');
       }
     };
 
     this.#endTime = Date.now() + duration;
-    requestAnimationFrame(updateDuration);
+    this.#rafId = requestAnimationFrame(updateDuration);
     this.#emitter.emit('onstart', duration);
   }
 
-  reset() {}
+  stop() {
+    if (!this.isEnded) {
+      this.#endTime = 0;
+      if (this.#rafId) cancelAnimationFrame(this.#rafId);
+      this.#emitter.emit('onstop');
+    }
+  }
 
   addListener<T extends keyof EventTypes>(eventName: T, handler: (...args: EventTypes[T]) => void) {
     this.#emitter.addListener(eventName, handler);
