b96acf1098438fe8b28ecaa3035be14be49de12b
mizdra <pp.mizdra@gmail.com>
2020-04-03 01:14:29 +0900

    jestでもbabelを通すように

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
   testMatch: ['<rootDir>/test/**/*.ts'],
 };
--- test/lib/timer.ts
+++ test/lib/timer.ts
@@ -1,3 +1,7 @@
+import { Timer } from '../../src/lib/timer';
+
 test('adds 1 + 2 to equal 3', () => {
+  const timer = new Timer();
+
   expect(1 + 2).toBe(3);
 });
