9912cb52001e583c6ce1478a0f0bde26151f38b0
mizdra <pp.mizdra@gmail.com>
2020-04-05 02:00:02 +0900

    format

--- babel.config.js
+++ babel.config.js
@@ -31,6 +31,6 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }]
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
   ],
 };
