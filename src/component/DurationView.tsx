4fe9c8bdcdbc8e6c9a1e5eca0d1a9f5b1f6bb6d6
mizdra <pp.mizdra@gmail.com>
2020-04-05 22:02:01 +0900

    日や時が省略可能なときは省略する

--- src/component/DurationView.tsx
+++ src/component/DurationView.tsx
@@ -17,14 +17,25 @@ export function DurationView({ value }: DurationViewProps) {
   const { days, hours, minutes, seconds, cs } = formatDuration(value);
   return (
     <div>
-      <span>{days}</span>
-      <span className={classes.unit}>日</span>
-      <span>{hours}</span>
-      <span className={classes.unit}>時間</span>
+      {days > 0 && (
+        <>
+          <span>{days}</span>
+          <span className={classes.unit}>日</span>
+        </>
+      )}
+      {hours > 0 && (
+        <>
+          <span>{hours}</span>
+          <span className={classes.unit}>時間</span>
+        </>
+      )}
+
       <span>{minutes.toString().padStart(2, '0')}</span>
       <span className={classes.unit}>分</span>
+
       <span>{seconds.toString().padStart(2, '0')}</span>
       <span className={classes.unit}>秒</span>
+
       <span>{cs.toString().padStart(2, '0')}</span>
     </div>
   );
--- src/lib/duration.ts
+++ src/lib/duration.ts
@@ -1,3 +1,7 @@
+/**
+ * 残り時間を日・時・分・秒・センチ秒に分解する
+ * @param duration 残り時間
+ * */
 export function formatDuration(duration: number) {
   return {
     days: Math.trunc(duration / 1000 / 60 / 60 / 24),
