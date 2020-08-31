d75e5b6f315e951b3a4ea5b021c8b62f9935deff
mizdra <pp.mizdra@gmail.com>
2020-08-31 21:46:01 +0900

    BigQueryに送信するロジックを実装

--- src/serverless/aggregate-memory-measurement.ts
+++ src/serverless/aggregate-memory-measurement.ts
@@ -1,22 +1,38 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
+import { BigQuery } from '@google-cloud/bigquery'
+
+const bigqueryClient = new BigQuery()
+const dataset = bigqueryClient.dataset('user_dataset')
+const table = dataset.table('memory_measurement')
+
+async function insertMemoryMeasurement (data: string) {
+  return new Promise((resolve, reject) => {
+    table.insert(data, (err, apiResponse) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(apiResponse)
+    })
+  })
+}
 
 // BigQuery にメモリ使用量を送信するためのプロキシ
 
-export const handler: APIGatewayProxyHandler = (
+export const handler: APIGatewayProxyHandler = async (
   event,
   _context,
-  callback,
 ) => {
   if (event.httpMethod !== 'POST') {
-    return callback(null, { statusCode: 405, body: 'Method Not Allowed' })
+    return { statusCode: 405, body: 'Method Not Allowed' }
   }
   if (event.body === null) {
-    return callback(null, { statusCode: 400, body: 'Bad Request' })
+    return { statusCode: 400, body: 'Bad Request' }
   }
   const data = JSON.parse(event.body)
-  console.log(data) // TODO: BigQuery に data を送信する
-  callback(null, {
+  console.log(data)
+  await insertMemoryMeasurement(event.body)
+  return {
     statusCode: 200,
-    body: 'Hello, World',
-  })
+    body: 'success',
+  }
 }
