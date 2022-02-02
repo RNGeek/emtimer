abb5d0ea860bea4b7d9c382bfbc2a50aed188d45
mizdra <pp.mizdra@gmail.com>
2022-02-03 01:27:39 +0900

    workbox-webpack-plugin の警告を解消

--- webpack.frontend.js
+++ webpack.frontend.js
@@ -130,7 +130,8 @@ module.exports = (env, argv) => ({
         },
       ],
     }),
-    new GenerateSW({
+    // workbox-webpack-plugin は watch ビルドには対応していないので、watch ビルドでは無効化する
+    argv.mode !== 'development' && new GenerateSW({
       cacheId: 'emtimer',
       swDest: resolve(distPath, './service-worker.js'),
       exclude: [/_redirects$/],
@@ -138,5 +139,5 @@ module.exports = (env, argv) => ({
       // TODO: チャンクを分割して上限の上書きをやめる
       maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
     }),
-  ],
+  ].filter(Boolean),
 })
